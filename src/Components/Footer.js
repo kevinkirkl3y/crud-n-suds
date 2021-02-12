4fe701b4e357f50cdaa8e227e67296791bf43078
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-02-12 09:39:03 -0800

    add components file and Header, Footer and Body Components.

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(){
+  return(
+    <>
+      <h2>Crud 'N' Suds</h2>
+      <br/>
+      <h3>Copyright 2021</h3>
+    </>
+  )
+}
+export default Footer;
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(){
+  return(
+    <>
+      <h1>Crud 'N' Suds</h1>
+    </>
+
+  ) 
+}
+
+export default Header;
